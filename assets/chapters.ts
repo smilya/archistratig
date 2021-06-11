3cf6085ba0b2ab3bf7e67c498578ff39bbe1a8fd
Ilya Smirnov <ilya_smirnov@epam.com>
2021-06-11 16:11:11 +0300

    Auto-generated commit [ci skip]

---
+++ assets/chapters.ts
@@ -0,0 +1,7 @@
+export const CHAPTERS = [
+  { id: 1, town: 'Москва', contactName: 'Мишка Косолапый', phone: '+7 901 101 11 11' },
+  { id: 2, town: 'Шуя', contactName: 'Хитрый Лис', phone: '+7 901 101 33 44' },
+  { id: 3, town: 'Сталинград', contactName: 'Злобный Хрюн', phone: '+7 901 145 78 91' },
+  { id: 4, town: 'Норильск', contactName: 'Важный Никель', phone: '+7 986 156 55 71' },
+  { id: 5, town: 'Саранск', contactName: 'Далекий Батя', phone: '+7 986 565 82 54' },
+];
